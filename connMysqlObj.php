c826a6d13b4f2c2b837a3cb67c746aaa1aac8e40
hyseanlin <hylin827@gmail.com>
2022-09-30 18:16:24 +0800

    Update the port number of MySQL.

--- connMysqlObj.php
+++ connMysqlObj.php
@@ -5,7 +5,7 @@ $db_username = "root";
 $db_password = "";
 $db_name = "nba";
 //連線資料庫
-$db_link = @new mysqli($db_host, $db_username, $db_password, $db_name, 3308);
+$db_link = @new mysqli($db_host, $db_username, $db_password, $db_name, 3306);
 //錯誤處理
 if ($db_link->connect_error != "") {
     echo "資料庫連結失敗！";
